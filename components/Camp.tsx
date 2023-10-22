cead5182d8a0fda8725564d1f7712eaf5268af89
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-10-22 13:57:28 -0300

    Guide section

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -54,7 +54,7 @@ const CampSite = ({
 function Camp() {
   return (
     <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
         <CampSite
           backgroundImage="bg-bg-img-1"
           title="Putuk Truno Camp"
